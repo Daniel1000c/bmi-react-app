9cb4eba969c65949c3326248bebf5fc0b85e1279
Daniel Velez <velezdaniel516@gmail.com>
2025-02-12 11:06:39 -0500

    create footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            
+
+            <p>© 2023 by Daniel Velez | All Rights Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer;
