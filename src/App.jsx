7215c2462329d2b79d13867e0ee29262ef079a8d
Daniel Velez <velezdaniel516@gmail.com>
2025-02-12 11:05:05 -0500

    add index.css import to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Footer from './components/footer';
+import './index.css';
 
 function App() {
 
   return (
-    <>
+    <div>
       <p>Hello World</p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
